937f51aa1478504b7c053ebfa5de9330a2b0af53
infoshreenilkanthstore-collab <info.shreenilkanthstore@gmail.com>
2026-09-03 10:30:46 +0530

    Update layout 1.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,74 +44,82 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Analytics (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0VTQFMW2S2"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0VTQFMW2S2');
-            `,
-          }}
-        />
+     <head>
+  {/* Google Analytics (gtag.js) */}
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-0VTQFMW2S2"
+  ></script>
 
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1455730108844531');
-              fbq('track', 'PageView');
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-0VTQFMW2S2');
+      `,
+    }}
+  />
 
-              // Global ViewContent helper
-              window.trackFbViewContent = function(productId, productName, productPrice) {
-                if (window.fbq) {
-                  fbq('track', 'ViewContent', {
-                    content_ids: [String(productId)],
-                    content_name: String(productName),
-                    content_type: 'product',
-                    value: Number(productPrice) || 0,
-                    currency: 'INR'
-                  });
-                }
-              };
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1455730108844531&ev=PageView&noscript=1"
-            alt="facebook pixel"
-          />
-        </noscript>
+  {/* Meta Pixel Code */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
 
-   <script type="text/javascript">
+        fbq('init', '1455730108844531');
+        fbq('track', 'PageView');
 
+        window.trackFbViewContent = function(productId, productName, productPrice) {
+          if (window.fbq) {
+            fbq('track', 'ViewContent', {
+              content_ids: [String(productId)],
+              content_name: String(productName),
+              content_type: 'product',
+              value: Number(productPrice) || 0,
+              currency: 'INR'
+            });
+          }
+        };
+      `,
+    }}
+  />
+
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: "none" }}
+      src="https://www.facebook.com/tr?id=1455730108844531&ev=PageView&noscript=1"
+      alt="facebook pixel"
+    />
+  </noscript>
+
+  {/* Microsoft Clarity */}
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+    {`
       (function(c,l,a,r,i,t,y){
-
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-
+        c[a]=c[a]||function(){
+          (c[a].q=c[a].q||[]).push(arguments)
+        };
+        t=l.createElement(r);
+        t.async=1;
+        t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "yccwjvvf0n");
+    `}
+  </Script>
+</head>
 
-  </script>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pb-16 md:pb-0`}
       >
